541852751a7b21c28e82871727ded1b7010fb987
Rodrigues <rdgs2309@gmail.com>
2023-06-20 09:34:59 -0300

    Adicinando

---
+++ src/components/Help/Help.js
@@ -0,0 +1,121 @@
+import React from "react";
+import "./style.css";
+const Help = () => {
+  return (
+    <section id="Help">
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>My Account</h1>
+          <p>
+            Make the appearance of a mobile application that has quality and
+            increases user convenience
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Shared Hosting</h1>
+          <p>
+            Help create a website and redesign it so that it becomes a website
+            that is more trendy and looks fresh
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Domain</h1>
+          <p>
+            Create customizable illustrations with attractive designs that are
+            made visually through high creativity
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>VPS</h1>
+          <p>
+            Change the appearance of a design into code that will be made into
+            an amazing website
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Cloud Hosting</h1>
+          <p>
+            Create 2d / 3d video animation in a short period of time designed to
+            promote a company product
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>WordPress Hosting</h1>
+          <p>Help your business grow with our SEO Experts</p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Referals</h1>
+          <p>
+            Change the appearance of a design into code that will be made into
+            an amazing website
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>cPanel</h1>
+          <p>
+            Create 2d / 3d video animation in a short period of time designed to
+            promote a company product
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>DNS</h1>
+          <p>Help your business grow with our SEO Experts</p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Billing</h1>
+          <p>
+            Change the appearance of a design into code that will be made into
+            an amazing website
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Website</h1>
+          <p>
+            Create 2d / 3d video animation in a short period of time designed to
+            promote a company product
+          </p>
+        </div>
+      </div>
+      <div className="div">
+        <div className="divImage"></div>
+        <div className="divInfo">
+          <h1>Customer Care</h1>
+          <p>Help your business grow with our SEO Experts</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Help;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../style.css";
 import { Head1 } from "../../components/HeaderMin/Headers";
+import Help from "../../components/Help/Help";
 import Cta from "../../components/CTA/Cta";
 import Footer from "../../components/Footer/Footer";
 const About = () => {
   return (
     <section id="page1">
       <Head1 />
+      <Help />
       <Cta />
       <Footer />
     </section>
